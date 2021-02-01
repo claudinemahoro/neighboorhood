8c2db45d06e0e19364e1bb92679bf3154ec1e558
claudinemahoro <claudinemah3@gmail.com>
2021-02-01 18:35:35 +0200

    views for creating profile and editing it

--- neighboor/views.py
+++ neighboor/views.py
@@ -69,3 +69,29 @@ def new_business(request):
     else:
         form1 = BusinessForm()
     return render(request, 'new_business.html', {"form1": form1})
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request, username=None):
+	'''
+	Method that fetches a users profile page
+	'''
+	current_user = request.user
+	pi_images = NeighbourHood.objects.filter(user=current_user)
+
+	return render(request,"profile.html",locals(),{"pi_images":pi_images})
+
+@login_required(login_url='/accounts/login/')
+def profile_edit(request):
+    current_user=request.user
+    if request.method=='POST':
+        form=ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            image=form.save(commit=False)
+            image.user=current_user
+            image.save()
+        return redirect('profile')
+    else:
+        form=ProfileForm()
+    return render(request,'profile_edit.html',{"form":form})
+
