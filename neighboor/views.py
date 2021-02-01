d4793f65f0b4bd89f5e528f69894bd06c115d86a
claudinemahoro <claudinemah3@gmail.com>
2021-02-01 18:33:17 +0200

    views for creating new hood,new post and new business

--- neighboor/views.py
+++ neighboor/views.py
@@ -28,3 +28,44 @@ def bus_details(request,business_id):
      details1=Business.get_specific_bus(business_id)
 
      return render(request,'new_business.html',{"details1":details1})
+def create_hood(request):
+    if request.method == 'POST':
+        form = NeighborForm(request.POST, request.FILES)
+        if form.is_valid():
+            hood = form.save(commit=False)
+            hood.admin = request.user
+            hood.save()
+            return redirect('welcome')
+    else:
+        form = NeighborForm()
+    return render(request, 'hood.html', {'form': form})
+@login_required(login_url='/accounts/login/')
+def new_post(request):
+    newpost = Post.objects.all()
+    current_user = request.user
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+        return redirect('hood')
+
+    else:
+        form = PostForm()
+    return render(request, 'new_post.html', {"form": form, "newpost": newpost})
+
+@login_required(login_url='/accounts/login/')
+def new_business(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form1 = BusinessForm(request.POST, request.FILES)
+        if form1.is_valid():
+            bus = form1.save(commit=False)
+            bus.user = current_user
+            bus.save()
+        return redirect('hood')
+
+    else:
+        form1 = BusinessForm()
+    return render(request, 'new_business.html', {"form1": form1})
