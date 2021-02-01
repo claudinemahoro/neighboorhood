e8ff8c58550bba6e07236fc2324cf205ea08fc09
claudinemahoro <claudinemah3@gmail.com>
2021-02-01 20:24:40 +0200

    business model

--- neighboor/models.py
+++ neighboor/models.py
@@ -41,4 +41,16 @@ class Profile(models.Model):
     def delete_profile(self):
         self.delete()
 
+class Business(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.EmailField(max_length=254)
+    description = models.TextField(blank=True)
+    # neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, related_name='business')
+    # user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='owner')
+    @classmethod
+    def get_specific_bus(cls,id):
+        bus = cls.objects.get(id=id)
+        return bus
+
+
 
