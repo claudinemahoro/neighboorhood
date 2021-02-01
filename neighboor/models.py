7a93e429cb791465607fd6d02d7de839d683c450
claudinemahoro <claudinemah3@gmail.com>
2021-02-01 20:23:33 +0200

    profile model

--- neighboor/models.py
+++ neighboor/models.py
@@ -26,4 +26,19 @@ class NeighbourHood(models.Model):
 
     def delete_hood(self):
         self.delete() 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(upload_to='images/')
+    bio = models.TextField(max_length=500)
+    contact = models.CharField(max_length=200)
+   
+    def __str__(self):
+        return self.bio
+    
+    def save_profile(self):
+        self.save()
+    
+    def delete_profile(self):
+        self.delete()
+
 
